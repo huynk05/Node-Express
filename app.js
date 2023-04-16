2c6b57cc4497b78c6e77928bd4638dd9e39280f3
huynk05 <huynk05@gmail.com>
2023-04-16 16:25:27 +0700

    92 - Working on the layout with Partials

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,12 @@ const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+  });
 });
 
 module.exports = router;
