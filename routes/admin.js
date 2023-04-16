74284d74fc6902215dbe854a3e10623811dda2e3
huynk05 <huynk05@gmail.com>
2023-04-16 17:00:53 +0700

    99 - Finish the cotrollers

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 const rootDir = require("../util/path");
@@ -7,18 +8,9 @@ const rootDir = require("../util/path");
 const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,11 @@
 const path = require("path");
 const express = require("express");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 const rootDir = require("../util/path");
 const adminData = require("./admin");
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
