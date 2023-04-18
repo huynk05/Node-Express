c1a2441a74907a9ffd90f0d56438951a7f21d594
huynk05 <huynk05@gmail.com>
2023-04-18 09:14:35 +0700

    109 - Register the routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 const rootDir = require("../util/path");
@@ -8,9 +8,12 @@ const rootDir = require("../util/path");
 const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", productsController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
